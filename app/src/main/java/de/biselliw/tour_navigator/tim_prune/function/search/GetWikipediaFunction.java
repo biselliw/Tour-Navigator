45815365b85c5fb8549ba95d82074c63d99ff9f0
Walter Biselli <walter.biselli@online.de>
2026-01-01 14:13:18 +0100

    opt

---
+++ app/src/main/java/de/biselliw/tour_navigator/tim_prune/function/search/GetWikipediaFunction.java
@@ -0,0 +1,121 @@
+package de.biselliw.tour_navigator.tim_prune.function.search;
+
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ArrayList;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import de.biselliw.tour_navigator.R;
+import de.biselliw.tour_navigator.activities.helper.BaseActivity;
+import de.biselliw.tour_navigator.tim_prune.data.DataPoint;
+import de.biselliw.tour_navigator.tim_prune.function.GetWikipediaXmlHandler;
+
+/**
+ * Function to load nearby point information from Wikipedia
+ * according to the currently viewed area
+ */
+public class GetWikipediaFunction extends GenericDownloaderFunction
+{
+	/** Maximum number of results to get */
+	private static final int MAX_RESULTS = 20;
+	/** Maximum distance from point in km */
+	private static final int MAX_DISTANCE = 15;
+	/** Username to use for geonames queries */
+	private static final String GEONAMES_USERNAME = "gpsprune";
+
+    private DataPoint dataPoint = null;
+    String lang = "";
+
+    BaseActivity _activity;
+	/**
+	 * Constructor
+	 * @param activity App object
+	 */
+	public GetWikipediaFunction(BaseActivity activity, TrackListModel inTrackListModel) {
+		super(null, inTrackListModel);
+        _activity = activity;
+	}
+
+	public void getWikipedia(DataPoint inPoint, String inLang)
+    {
+        dataPoint = inPoint;
+        lang = inLang;
+        new Thread(() -> {
+            // background work
+            run();
+        }).start();
+    }
+
+	/**
+	 * Run method to get the nearby points in a separate thread
+	 */
+	public void run()
+	{
+		// Get coordinates from current point (if any) or from centre of screen
+        if(dataPoint == null) return;
+        double lat = dataPoint.getLatitude().getDouble();
+        double lon = dataPoint.getLongitude().getDouble();
+
+		// For geonames, firstly try the local language
+        submitSearch(lat, lon, lang);
+
+        // prohibit recursive search
+        dataPoint = null;
+
+		// Set status label according to error or "none found", leave blank if ok
+		if (_errorMessage.isEmpty() && _trackListModel.isEmpty()) {
+			_errorMessage = _activity.getString(R.string.wikipedia_articles_nonefound);
+		}
+	}
+
+	/**
+	 * Submit the search for the given parameters
+	 * @param inLat latitude
+	 * @param inLon longitude
+	 * @param inLang language code to use, such as en or de
+	 */
+	private void submitSearch(double inLat, double inLon, String inLang)
+	{
+		// Example http://api.geonames.org/findNearbyWikipedia?lat=47&lng=9
+		String urlString = "https://secure.geonames.org/findNearbyWikipedia?lat=" +
+			inLat + "&lng=" + inLon + "&maxRows=" + MAX_RESULTS
+			+ "&radius=" + MAX_DISTANCE + "&lang=" + inLang
+			+ "&username=" + GEONAMES_USERNAME;
+		// Parse the returned XML with a special handler
+        GetWikipediaXmlHandler xmlHandler = new GetWikipediaXmlHandler();
+		InputStream inStream = null;
+        _errorMessage = "";
+
+		try
+		{
+			URL url = new URL(urlString);
+			SAXParser saxParser = SAXParserFactory.newInstance().newSAXParser();
+			inStream = url.openStream();
+			saxParser.parse(inStream, xmlHandler);
+		}
+		catch (Exception e) {
+			_errorMessage = e.getClass().getName() + " - " + e.getMessage();
+		}
+		// Close stream and ignore errors
+		try {
+			inStream.close();
+		} catch (Exception ignored) {}
+		// Add track list to model
+		ArrayList<SearchResult> trackList = xmlHandler.getTrackList();
+
+        if (_trackListModel != null) {
+            _trackListModel.addTracks(trackList, true);
+
+            // Show error message if any
+            if (_trackListModel.isEmpty()) {
+                String error = xmlHandler.getErrorMessage();
+                if (error != null && !error.equals("")) {
+                    //	_app.showErrorMessageNoLookup(getNameKey(), error);
+                    _errorMessage = error;
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/de/biselliw/tour_navigator/tim_prune/function/search/TrackListModel.java
@@ -0,0 +1,163 @@
+package de.biselliw.tour_navigator.tim_prune.function.search;
+
+import java.text.NumberFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+
+// import javax.swing.table.AbstractTableModel;
+
+// import tim.prune.I18nManager;
+// import tim.prune.config.Config;
+import de.biselliw.tour_navigator.stubs.Config;
+import tim.prune.data.Unit;
+import de.biselliw.tour_navigator.tim_prune.function.search.SearchResult;
+
+/**
+ * Model for list of tracks from a search result (eg geonames, overpass)
+ */
+public class TrackListModel // extends AbstractTableModel
+{
+	/** List of tracks */
+	private ArrayList<SearchResult> _trackList = null;
+	/** Column heading for track name */
+	private String _nameColLabel = null;
+	/** Column heading for length */
+	private String _lengthColLabel = null;
+	/** Number of columns */
+	private int _numColumns = 2;
+	/** Normally this model shows distances / lengths, except when this flag is true */
+	private boolean _showPointTypes = false;
+	/** Formatter for distances */
+	private NumberFormat _distanceFormatter = NumberFormat.getInstance();
+
+    /**BiselliW */
+    public boolean changed = false;
+
+	/**
+	 * Constructor
+	 * @param inColumn1Key key for first column
+	 * @param inColumn2Key key for second column
+	 */
+	public TrackListModel(String inColumn1Key, String inColumn2Key)
+	{
+        /*
+
+		_nameColLabel = I18nManager.getText(inColumn1Key);
+		if (inColumn2Key != null) {
+			_lengthColLabel = I18nManager.getText(inColumn2Key);
+		}
+         */
+		_numColumns = (_lengthColLabel != null?2:1);
+		_distanceFormatter.setMaximumFractionDigits(1);
+	}
+
+	/**
+	 * @return column count
+	 */
+	public int getColumnCount()
+	{
+		return _numColumns;
+	}
+
+	/**
+	 * @return number of rows
+	 */
+	public int getRowCount()
+	{
+		if (_trackList == null) return 0;
+		return _trackList.size();
+	}
+
+	/** @return true if there are no rows */
+	public boolean isEmpty()
+	{
+		return getRowCount() == 0;
+	}
+
+	/**
+	 * @param inColNum column number
+	 * @return column label for given column
+	 */
+	public String getColumnName(int inColNum)
+	{
+		if (inColNum == 0) {return _nameColLabel;}
+		return _lengthColLabel;
+	}
+
+	/**
+	 * @param inShowTypes true to show point types, false for distances
+	 */
+	public void setShowPointTypes(boolean inShowTypes)
+	{
+		_showPointTypes = inShowTypes;
+	}
+
+	/**
+	 * @param inRowNum row number
+	 * @param inColNum column number
+	 * @return cell entry at given row and column
+	 */
+	public Object getValueAt(int inRowNum, int inColNum)
+	{
+		SearchResult track = _trackList.get(inRowNum);
+		if (inColNum == 0) {
+			return track.getTrackName();
+		}
+		if (_showPointTypes)
+		{
+			return track.getPointType();
+		}
+		double lengthM = track.getLength();
+		// convert to current distance units
+		Unit distUnit = Config.getUnitSet().getDistanceUnit();
+		double length = lengthM * distUnit.getMultFactorFromStd();
+		// Make text
+		return _distanceFormatter.format(length) + " km";
+	}
+
+	/**
+	 * Add a list of tracks to this model
+	 * @param inList list of tracks to add
+	 */
+	public void addTracks(ArrayList<SearchResult> inList)
+	{
+		addTracks(inList, false);
+	}
+
+	/**
+	 * Add a list of tracks to this model and optionally sort them
+	 * @param inList list of tracks to add
+	 * @param inSort true to sort results after adding
+	 */
+	public void addTracks(ArrayList<SearchResult> inList, boolean inSort)
+	{
+		if (_trackList == null) {_trackList = new ArrayList<SearchResult>();}
+		final int prevCount = _trackList.size();
+		if (inList != null && inList.size() > 0)
+		{
+			_trackList.addAll(inList);
+			if (inSort) {
+				Collections.sort(_trackList);
+			}
+		}
+		final int updatedCount = _trackList.size();
+        changed = true;
+	}
+
+	/**
+	 * @param inRowNum row number from 0
+	 * @return track object for this row
+	 */
+	public SearchResult getTrack(int inRowNum)
+	{
+		return _trackList.get(inRowNum);
+	}
+
+	/**
+	 * Clear the list of tracks
+	 */
+	public void clear()
+	{
+		_trackList = null;
+	}
+}
